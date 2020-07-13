6404e7434b8ad58819da307b415211c4f3d31ddd
Fedor <fedorskvorcov@yandex.ru>
2020-07-13 16:39:16 +0300

    Add recycler view itemClicks

--- app/src/main/java/com/skvortsovfk/bluetoothlescanner/adapters/DeviceListAdapter.kt
+++ app/src/main/java/com/skvortsovfk/bluetoothlescanner/adapters/DeviceListAdapter.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.skvortsovfk.bluetoothlescanner.databinding.ListItemDeviceBinding
+import kotlinx.android.synthetic.main.list_item_device.view.*
 
 class DeviceListAdapter : RecyclerView.Adapter<DeviceViewHolder>() {
+
+    var onItemClick: ((BluetoothDevice) -> Unit)? = null
+    var onNameClick: ((BluetoothDevice) -> Unit)? = null
 
     var values = emptyList<BluetoothDevice>()
         set(value) {
@@ -27,6 +31,13 @@ class DeviceListAdapter : RecyclerView.Adapter<DeviceViewHolder>() {
     override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
         val device = values[position]
         holder.bind(device)
-    }
 
+        holder.itemView.cardView.setOnClickListener {
+            onItemClick?.invoke(values[position])
+        }
+
+        holder.itemView.cardView.nameTextView.setOnClickListener {
+            onNameClick?.invoke(values[position])
+        }
+    }
 }
--- app/src/main/java/com/skvortsovfk/bluetoothlescanner/ui/DeviceListFragment.kt
+++ app/src/main/java/com/skvortsovfk/bluetoothlescanner/ui/DeviceListFragment.kt
@@ -1,10 +1,12 @@
 package com.skvortsovfk.bluetoothlescanner.ui
 
+import android.bluetooth.BluetoothDevice
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.skvortsovfk.bluetoothlescanner.R
@@ -37,6 +39,22 @@ class DeviceListFragment : Fragment() {
 
         val adapter = DeviceListAdapter()
         binding.deviceListRecyclerView.adapter = adapter
+
+        adapter.onItemClick = { bluetoothDevice ->
+            Toast.makeText(
+                context,
+                "Clicked ${bluetoothDevice.name} ${bluetoothDevice.address}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        adapter.onNameClick = { bluetoothDevice ->
+            Toast.makeText(
+                context,
+                "${bluetoothDevice.name}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
         viewModel.devices.observe(viewLifecycleOwner, Observer {  devices ->
             devices?.let { adapter.values = devices }
